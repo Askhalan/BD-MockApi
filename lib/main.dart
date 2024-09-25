1ce266f39cfc7af017556a1566c2300972420d4a
Askhalan A <askhalan.aj@gmail.com>
2024-09-25 11:45:06 +0530

    create folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
+import 'package:bd_mock_api/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp();
-  }
-}
